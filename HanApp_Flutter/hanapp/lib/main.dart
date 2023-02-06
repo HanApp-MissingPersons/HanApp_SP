cf71031fb4c7fea7ebbf78b6acbfa5c73fd33f31
Emmanuel Guides <eguides@up.edu.ph>
2023-02-06 14:04:57 +0800

    main and companion directories

--- HanApp_Flutter/hanapp/lib/main.dart
+++ HanApp_Flutter/hanapp/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // LANDING PAGE HERE
       home: const RegisterView(),
     );
   }
