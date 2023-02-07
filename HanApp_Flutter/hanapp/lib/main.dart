87ce2fb5b0022a66ecf9fc700bf242af0152f353
Emmanuel Guides <eguides@up.edu.ph>
2023-02-07 17:34:22 +0800

    login, verify email, register overhaul

--- HanApp_Flutter/hanapp/lib/main.dart
+++ HanApp_Flutter/hanapp/lib/main.dart
@@ -25,80 +25,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // LANDING PAGE HERE
-      home: const RegisterView(),
+      home: const LoginView(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(heightFactor: 240, widthFactor: 240, child: Text('HOMEPAGE')) ,
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState){
-            case ConnectionState.none:
-              return const Center(child: Text('None oh no'));
-            case ConnectionState.waiting:
-              return const Center(child: Text('Loading . . .'));
-            case ConnectionState.active:
-              return const Center(child: Text('App loading in...'));
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              final emailVerifiedCheck = user?.emailVerified ?? false;
-              if(emailVerifiedCheck) {
-                if (kDebugMode) {
-                  print('[VERIFIED] User is Verified');
-                }
-                return const Center(child: Text('BRO DONEZA'));
-              }
-              else {
-                if (kDebugMode) {
-                  print('[UNVERIFIED] User is not verified');
-                }
-                Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const VerifyEmailView(),
-                    )
-                );
-              }
-              return const Center(child: Text('Verify your email first'));
-            default:
-              return const Center(child: Text('Loading . . . '));
-          }
-        },
-      ),
-    );
-  }
-}
-
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({Key? key}) : super(key: key);
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // TODO: 11:25:21
-      appBar: AppBar(
-        title: const Center(child: Text('Verify Email')),
-      ),
-      body: const Center(child: Text('verify email here')),
-    );
-  }
-}
 
 
 
---
+++ HanApp_Flutter/hanapp/lib/views/verify_email_view.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Text('Verify your email first:'),
+      TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null && !user.emailVerified) {
+              await user.sendEmailVerification();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Verification email sent'),
+                ),
+              );
+            }
+          },
+          child: const Text('Send Email Verification')
+      )
+    ],);
+  }
+}
