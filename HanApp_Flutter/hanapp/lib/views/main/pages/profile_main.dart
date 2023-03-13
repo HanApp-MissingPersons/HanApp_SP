2f554d92a5bc4c71a6827f6b8d0aad63118c5e71
Emmanuel Guides <eguides@up.edu.ph>
2023-03-13 21:06:39 +0800

    displaying current user information on userprofile

--- HanApp_Flutter/hanapp/lib/views/main/pages/profile_main.dart
+++ HanApp_Flutter/hanapp/lib/views/main/pages/profile_main.dart
@@ -10,13 +10,32 @@ class ProfileMain extends StatefulWidget {
   State<ProfileMain> createState() => _ProfileMain();
 }
 
+// class User(var name, var email, var phone, var user){
+//   late String name;
+//   late String email;
+//   late String phone;
+//   User(){
+//     name = name;
+//     email = email;
+//     phone = phone;
+//   }
+// }
+
 class _ProfileMain extends State<ProfileMain> {
   // ignore: prefer_typing_uninitialized_variables
   late var user;
+  late String fullName = 'Loading...';
   late DatabaseReference mainUserRef;
   _ProfileMain() {
     user = FirebaseAuth.instance.currentUser;
     mainUserRef = FirebaseDatabase.instance.ref('Main Users').child(user!.uid);
+    mainUserRef.onValue.listen((event) {
+      var usersData = Map<String, dynamic>.from(
+          event.snapshot.value as Map<dynamic, dynamic>);
+      for (var element in usersData.values) {
+        fullName = element['fullName'];
+      }
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -30,7 +49,7 @@ class _ProfileMain extends State<ProfileMain> {
             Container(
                 padding: EdgeInsets.only(
                     top: MediaQuery.of(context).size.height / 3),
-                child: Text("$user")),
+                child: Text('Current User data: \n\n$user')),
             Container(
               padding: const EdgeInsets.all(10),
               child: ElevatedButton(
