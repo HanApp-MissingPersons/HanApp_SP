935873f192275048be02b21b999479f432764578
Emmanuel Guides <eguides@up.edu.ph>
2023-03-17 07:33:34 +0800

    read and display user info from RTDB

--- HanApp_Flutter/hanapp/lib/views/main/pages/profile_main.dart
+++ HanApp_Flutter/hanapp/lib/views/main/pages/profile_main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hanapp/views/login_view.dart';
@@ -16,73 +17,77 @@ class ProfileMain extends StatefulWidget {
 }
 
 class _ProfileMain extends State<ProfileMain> {
-  // DatabaseReference mainUserRef =
-  //     FirebaseDatabase.instance.ref('Main Users').child(user.uid);
+  String _usrFullName = ' ';
+  String _usrEmail = ' ';
+  String _usrNumber = ' ';
+  DatabaseReference mainUsersRef = FirebaseDatabase.instance.ref('Main Users');
+  final user = FirebaseAuth.instance.currentUser;
 
-  late final Future<FirebaseApp> _firebaseInit;
   @override
   void initState() {
-    _firebaseInit = Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
     super.initState();
+    _activateListeners();
   }
+
+  void _activateListeners() {
+    // user profile details from RTDB
+    mainUsersRef.child(user!.uid).onValue.listen((event) {
+      var usrProfileDict = Map<String, dynamic>.from(
+          event.snapshot.value as Map<dynamic, dynamic>);
+      String nameFromDB = usrProfileDict['fullName'];
+      String numberFromDB = usrProfileDict['phoneNumber'];
+      String emailFromDB = usrProfileDict['email'];
+      if (kDebugMode) {
+        print('[RETRIEVED] $usrProfileDict');
+      }
+
+      setState(() {
+        _usrFullName = nameFromDB;
+        _usrNumber = numberFromDB;
+        _usrEmail = emailFromDB;
+      });
+    });
+  }
+
+  // void setUserDetails(data) {
+  //   setState(() {
+  //     usersData = data;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    // // var usersData;
+    // mainUserRef.onValue.listen((event) {
+    //   final usersData = Map<String, dynamic>.from(
+    //       event.snapshot.value as Map<dynamic, dynamic>);
+    // });
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Balls'),
+      appBar: AppBar(
+        title: const Text('profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_usrFullName),
+            Text(_usrEmail),
+            Text(_usrNumber),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginView()));
+                  },
+                  child: const Text('Sign Out')),
+            ),
+          ],
         ),
-        body: FutureBuilder(
-          future: _firebaseInit,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Center(child: Text('Oops! something went wrong!'));
-            } else {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return const Center(child: Text('None oh no'));
-                case ConnectionState.waiting:
-                  return const Center(
-                    child: Center(
-                      child: SpinKitCubeGrid(
-                        color: Palette.indigo,
-                        size: 50.0,
-                      ),
-                    ),
-                  );
-                case ConnectionState.active:
-                  return const Center(child: Text('Connection active.'));
-                case ConnectionState.done:
-                  final user = FirebaseAuth.instance.currentUser;
-                  DatabaseReference mainUserRef = FirebaseDatabase.instance
-                      .ref('Main Users')
-                      .child(user!.uid);
-
-                  return Column(
-                    children: [
-                      Container(
-                          padding: EdgeInsets.only(
-                              top: MediaQuery.of(context).size.height / 3),
-                          child: Text('Current User data: \n\n$mainUserRef')),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        child: ElevatedButton(
-                            onPressed: () {
-                              FirebaseAuth.instance.signOut();
-                              Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const LoginView()));
-                            },
-                            child: const Text('Sign Out')),
-                      )
-                    ],
-                  );
-              }
-            }
-          },
-        ));
+      ),
+    );
   }
 }
