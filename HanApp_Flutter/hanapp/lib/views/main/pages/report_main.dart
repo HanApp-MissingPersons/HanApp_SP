dec6b8cc841bbcbe5236c4ba44e303e3a47e31f3
Nikko Gabriel <90045936+nikko-gabriel@users.noreply.github.com>
2023-03-17 12:26:56 +0800

    fixed report_main, to display pages

--- HanApp_Flutter/hanapp/lib/views/main/pages/report_main.dart
+++ HanApp_Flutter/hanapp/lib/views/main/pages/report_main.dart
@@ -14,62 +14,7 @@ class ReportMain extends StatefulWidget {
   State<ReportMain> createState() => _ReportMainState();
 }
 
-////////// PageView WITHOUT Indicator
-class _ReportMainState extends State<ReportMain> {
-  // optionStyle is for the text, we can remove this when actualy doing menu contents
-  static const TextStyle optionStyle = TextStyle(
-      fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black54);
-
-  // initialize controller for PageView
-  final PageController _pageController = PageController(initialPage: 0);
-
-  // int _activePage = 0;
-
-  // //Create list holding all the pages
-  // final List<Widget> _ReportPages = [Page1Classifier()];
-
-  // dispose the controller, this is needed to avoid memory leak
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  // build the page view
-  @override
-  Widget build(BuildContext context) {
-    //use stack and sizedbox to make the pageview full screen
-    return Stack(children: [
-      Positioned(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: PageView(
-            controller: _pageController,
-            onPageChanged: (int page) {
-              setState(() {
-                // _activePage = page;
-              });
-            },
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              // set children to be centered
-              const Center(child: Page1Classifier()),
-              const Center(child: Page2ReporteeDetails()),
-              const Center(child: Page3MPDetails()),
-              const Center(child: Page4MPDesc()),
-              const Center(child: Page5IncidentDetails()),
-              const Center(child: Page6AuthConfirm()),
-            ],
-          ),
-        ),
-      )
-    ]);
-  }
-}
-///////// END PageView WITHOUT Indicator
-
-// ////// PageView with Bottom Incidator
+// ////////// PageView WITHOUT Indicator
 // class _ReportMainState extends State<ReportMain> {
 //   // optionStyle is for the text, we can remove this when actualy doing menu contents
 //   static const TextStyle optionStyle = TextStyle(
@@ -78,17 +23,10 @@ class _ReportMainState extends State<ReportMain> {
 //   // initialize controller for PageView
 //   final PageController _pageController = PageController(initialPage: 0);
 
-//   int _activePage = 0;
+//   // int _activePage = 0;
 
-//   //Create list holding all the pages
-//   final List<Widget> _ReportPages = [
-//     Page1Classifier(),
-//     Page2ReporteeDetails(),
-//     Page3MPDetails(),
-//     Page4MPDesc(),
-//     Page5IncidentDetails(),
-//     Page6AuthConfirm()
-//   ];
+//   // //Create list holding all the pages
+//   // final List<Widget> _ReportPages = [Page1Classifier()];
 
 //   // dispose the controller, this is needed to avoid memory leak
 //   @override
@@ -102,26 +40,94 @@ class _ReportMainState extends State<ReportMain> {
 //   Widget build(BuildContext context) {
 //     //use stack and sizedbox to make the pageview full screen
 //     return Stack(children: [
-//       Center(
+//       Positioned(
 //         child: SizedBox(
-//             width: MediaQuery.of(context).size.width,
-//             height: MediaQuery.of(context).size.height,
-//             child: Row(
-//               children: [
-//                 PageView.builder(
-//                     controller: _pageController,
-//                     onPageChanged: (int index) {
-//                       setState(() {
-//                         _activePage = index;
-//                       });
-//                     },
-//                     itemCount: _ReportPages.length,
-//                     itemBuilder: (BuildContext context, int index) {
-//                       return _ReportPages[index];
-//                     })
-//               ],
-//             )), // SizedBox
-//       ) // Positioned
-//     ]); // Stack
+//           width: MediaQuery.of(context).size.width,
+//           height: MediaQuery.of(context).size.height,
+//           child: PageView(
+//             controller: _pageController,
+//             onPageChanged: (int page) {
+//               setState(() {
+//                 // _activePage = page;
+//               });
+//             },
+//             // ignore: prefer_const_literals_to_create_immutables
+//             children: [
+//               // set children to be centered
+//               const Center(child: Page1Classifier()),
+//               const Center(child: Page2ReporteeDetails()),
+//               const Center(child: Page3MPDetails()),
+//               const Center(child: Page4MPDesc()),
+//               const Center(child: Page5IncidentDetails()),
+//               const Center(child: Page6AuthConfirm()),
+//             ],
+//           ),
+//         ),
+//       )
+//     ]);
 //   }
 // }
+// ///////// END PageView WITHOUT Indicator
+
+////// PageView with Bottom Incidator
+class _ReportMainState extends State<ReportMain> {
+  // optionStyle is for the text, we can remove this when actualy doing menu contents
+  static const TextStyle optionStyle = TextStyle(
+      fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black54);
+
+  // initialize controller for PageView
+  final PageController _pageController = PageController(initialPage: 0);
+
+  int _activePage = 0;
+
+  //Create list holding all the pages
+  final List<Widget> _ReportPages = [
+    Page1Classifier(),
+    Page2ReporteeDetails(),
+    Page3MPDetails(),
+    Page4MPDesc(),
+    Page5IncidentDetails(),
+    Page6AuthConfirm()
+  ];
+
+  // dispose the controller, this is needed to avoid memory leak
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  // build the page view
+  @override
+  Widget build(BuildContext context) {
+    //use stack and sizedbox to make the pageview full screen
+    return Stack(children: [
+      Center(
+        child: SingleChildScrollView(
+          child: Container(
+              // margin 1/8th of the screen height
+              margin:
+                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 8),
+              // avoid RenderBox was not laid out: RenderConstrainedBox#f2f9d NEEDS-LAYOUT NEEDS-PAINT using Expanded
+
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: PageView.builder(
+                  // define height of the pageview
+                  controller: _pageController,
+                  onPageChanged: (int index) {
+                    setState(() {
+                      _activePage = index;
+                    });
+                  }, // onPageChanged
+                  itemCount: _ReportPages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _ReportPages[index];
+                  } // itemBuilder
+                  ) // Expanded
+              ),
+        ), // SizedBox
+      ) // Positioned
+    ]); // Stack
+  }
+}
