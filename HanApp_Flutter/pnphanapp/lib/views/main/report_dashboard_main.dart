28bbce600634526e54c610e8e8ed83a606319a93
Erru Torculas <egtorculas@up.edu.ph>
2023-03-28 17:57:41 +0800

    added the UI for PNP login

---
+++ HanApp_Flutter/pnphanapp/lib/views/main/report_dashboard_main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DashboardMain extends StatefulWidget {
+  @override
+  _DashboardMainState createState() => _DashboardMainState();
+}
+
+class _DashboardMainState extends State<DashboardMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Container(
+          height: 80,
+          width: 150,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Welcome',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
