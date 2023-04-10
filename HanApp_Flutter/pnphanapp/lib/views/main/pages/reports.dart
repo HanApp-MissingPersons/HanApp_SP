ab67247566978ffa87c3af485cc9633b51651fcb
Emmanuel Guides <eguides@up.edu.ph>
2023-04-10 14:02:07 +0800

    retrieving as list (to be fixed)

--- HanApp_Flutter/pnphanapp/lib/views/main/pages/reports.dart
+++ HanApp_Flutter/pnphanapp/lib/views/main/pages/reports.dart
@@ -15,8 +15,50 @@ class reportsPNP extends StatefulWidget {
 
 // ignore: camel_case_types
 class _reportsPNPState extends State<reportsPNP> {
+  Query dbRef = FirebaseDatabase.instance.ref().child('Reports');
+
+  Widget listItem({required Map report}) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 110,
+      color: Colors.purple.shade100,
+      child: Column(
+        children: [
+          const Text('Reportee name'),
+          GestureDetector(
+            onTap: () {
+              print('Tapped');
+            },
+            child: Container(
+              margin: const EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.purple.shade300,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Text('View Report'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Reports PNP firebase initialized.');
+    return Container(
+        height: double.infinity,
+        child: FirebaseAnimatedList(
+          query: dbRef,
+          itemBuilder: (BuildContext context, DataSnapshot snapshot,
+              Animation<double> animation, int index) {
+            dynamic values = snapshot.value;
+            print('[values] ${values[0]}');
+            // Map report = snapshot.value as Map;
+            // return listItem(report: report);
+            return (Text('balls'));
+          },
+        ));
   }
 }
