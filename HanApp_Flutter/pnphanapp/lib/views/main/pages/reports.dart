e839bd3a6ac17092bb2d7aaa9b95bf8d1b403958
Emmanuel Guides <eguides@up.edu.ph>
2023-04-10 12:17:03 +0800

    changed menu item PNP

---
+++ HanApp_Flutter/pnphanapp/lib/views/main/pages/reports.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class reportsPNP extends StatefulWidget {
+  const reportsPNP({super.key});
+
+  @override
+  State<reportsPNP> createState() => _reportsPNPState();
+}
+
+class _reportsPNPState extends State<reportsPNP> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Reports PAGEadljwajhkahd.w.hdklahdkahslkE');
+  }
+}
