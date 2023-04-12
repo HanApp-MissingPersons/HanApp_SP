50740166408b6d73f1cc39b94691f9e5758cb045
Nikko Gabriel <90045936+nikko-gabriel@users.noreply.github.com>
2023-04-12 13:38:27 +0800

    added retrievePrefsData p6

--- HanApp_Flutter/hanapp/lib/views/main/pages/report_pages/p6_auth_confirm.dart
+++ HanApp_Flutter/hanapp/lib/views/main/pages/report_pages/p6_auth_confirm.dart
@@ -310,6 +310,7 @@ class _Page6AuthConfirmState extends State<Page6AuthConfirm> {
                                 await signaturePadKey.currentState!.toImage();
                             await _getSignature(image);
                             await _saveSignature();
+                            await retrievePrefsData();
 
                             // pop-up showing preview of signature
                             // ignore: use_build_context_synchronously
